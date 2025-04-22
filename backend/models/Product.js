5a5a6069053ea9d1e7f7019c4436910724d3449a
Shifa Shaikh <shifaas0809@gmail.com>
2025-04-22 08:17:00 +0530

    Add backend/models/Product.js 1745290019691

---
+++ backend/models/Product.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    default: '',
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+}, { timestamps: true });
+
+const Product = mongoose.model('Product', productSchema);
+
+module.exports = Product;
